a70b2ba637bb276f01b51a5f7f624741498f1085
Brenden Moore <brendenm3603@gmail.com>
2022-10-18 00:07:54 -0700

    added functions to javascript

--- apps.js
+++ apps.js
@@ -1,17 +1,33 @@
-let nameSpace = prompt("Hey! Whats your name?");
-document.write("Whats up ", nameSpace);
-console.log(nameSpace);
+'use strict';
 
-if (nameSpace == "Brenden"){
-} else if (nameSpace == "brenden"){
-  document.write(", the almighty creator")
+let usersName;
+
+function greeting(){
+  usersName = prompt("Hey! Whats your name?");
+  console.log("Whats good " + usersName);
+  document.write("Whats up " + usersName);
+  return usersName;
+} 
+
+greeting();
+
+if (usersName == "Brenden"){
+} else if (usersName == "brenden"){
+  document.write(", the almighty creator");
 }
-console.log(nameSpace);
 
-if (confirm("Do you own a Switch?")) {
+let userAnswer;
+
+function switchYesNo(){
+  if (confirm("Do you own a Switch?")) {
     document.write(". Thats great! Add my friend code: SW-8160-7494-2138");
     alert ("Nice!");
-}  else {
+  }  else {
     document.write(". I hope this page will convince you to get a Switch!");
 }
-console.log("Do you own a Switch?");
+console.log("Switch question");
+}
+
+switchYesNo();
+
+
