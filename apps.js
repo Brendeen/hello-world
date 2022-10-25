9b163459154348e08e9202f77663a251a8660b23
Brenden Moore <brendenm3603@gmail.com>
2022-10-24 18:56:32 -0700

    added loop question

--- apps.js
+++ apps.js
@@ -3,47 +3,44 @@
 let usersName;
 
 function greeting(){
-  usersName = prompt("Hey! Whats your name?");
-  if (usersName == ""){
-   usersName == prompt ("Please enter your name.");
-  }
-  console.log("Whats good " + usersName);
-  document.write("Whats up " + usersName);
+  usersName = prompt('Hey! Whats your name?');
+  document.write('Whats up ' + usersName)
+  console.log('Whats good ' + usersName);
   return usersName;
 } 
 
 greeting();
 
-if (usersName == "Brenden"){
-} else if (usersName == "brenden"){
-  document.write(", the almighty creator");
+if (usersName == 'Brenden'){
+} else if (usersName == 'brenden'){
+  document.write(', the almighty creator');
 }
 
 let userAnswer;
 
 function switchYesNo(){
-  if (confirm("Do you own a Switch?")) {
-    document.write(". Thats great! Add my friend code: SW-8160-7494-2138");
-    alert ("Nice!");
+  if (confirm('Do you own a Switch?')) {
+    document.write('. Thats great! Add my friend code: SW-8160-7494-2138');
+    alert ('Nice!');
   }  else {
-    document.write(". I hope this page will convince you to get a Switch!");
+    document.write('. I hope this page will convince you to get a Switch!');
 }
-console.log("Switch question");
+console.log('Switch question');
 }
 
 switchYesNo();
 
-let userAnswerTwo;
+let correctAnswer = 'bowser'
+let userAnswerTwo
 
-// function secondQuestion(){
- // userAnswerTwo = prompt("Quick Mario question, who is Marios greatest enemy?");
-  // if (userAnswerTwo == "Bowser")
-  // alert ("Thats correct!");
-  // else if (userAnswerTwo == "bowser")
-  // alert ("Thats correct!");
-  // else 
-  // alert ("Not quite. Try again.");
+function questionTwo(){
+ let userAnswerTwo = prompt('Who is Super Marios greatest enemy?');{
   
-// }
+  while (userAnswerTwo === 'bowser');
+     userAnswerTwo = prompt('incorrect...');
+  }
+  console.log(userAnswerTwo);
+  return userAnswerTwo
+}
 
-// secondQuestion();
+questionTwo();
